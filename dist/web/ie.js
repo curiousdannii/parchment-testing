ab4882a2fee135cb70aed1bedca2aa35f1fd5d24
curiousdannii <curiousdannii@gmail.com>
2022-06-24 02:04:27 +0000

    Deploying to gh-pages from @ curiousdannii/parchment@8ea14ff65c0fb592b1474f92836b1cf85dfbc283 🚀

--- dist/web/ie.js
+++ dist/web/ie.js
@@ -1,2 +1,2 @@
-function r(){$("#errorcontent").text("Sorry, but Parchment depends on web features unsupported by this browser. Please try a more modern browser."),$("#errorpane").show()}$(r);
+"use strict";function r(){$("#errorcontent").text("Sorry, but Parchment depends on web features unsupported by this browser. Please try a more modern browser."),$("#errorpane").show()}$(r);
 //# sourceMappingURL=ie.js.map
